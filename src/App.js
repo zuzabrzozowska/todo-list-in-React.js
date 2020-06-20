18ac48fa91e3598277a1f9daf6ee6c56786f4705
zuzanna_brzozowska <zuzabrzozowska@gmail.com>
2020-06-20 17:20:45 +0200

    deleted Modules, added ajax

--- src/App.js
+++ src/App.js
@@ -1,55 +1,113 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header/Header.js';
-import List from './Components/List/List.js';
-import Modal from './Components/Modal/Modal.js';
 import axios from 'axios';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       todoList: [],
-      newTask: {
-        title: '',
-      }
+      newTask: ''
     }
   }
 
   getAndRenderTasks = () => {
     axios.get('http://51.75.120.145:3000/todo')
     .then(resp => {
-      const tasksFromServer = resp.data;
-      this.setState({todoList: tasksFromServer});
-      console.log(this.state.todoList);
-    }).catch(error => {
-      console.log(error);
+      this.setState({todoList: resp.data});
     })
   }
+  postAndRenderTasks = (newTask) => {
+    axios.post('http://51.75.120.145:3000/todo', {
+      title: newTask
+    }).then(() => {
+      this.getAndRenderTasks();
+    })
+  }
+  deleteAndRenderTasks = (id) => {
+    axios.delete(`http://51.75.120.145:3000/todo/${id}`)
+    .then(() => {
+      this.getAndRenderTasks();
+    })
+  }
+  editAndRenderTasks = (id, newTask) => {
+    axios.put(`http://51.75.120.145:3000/todo/${id}, 
+    { title: ${newTask} }`)
+    .then(() => {
+      this.getAndRenderTasks();
+    })
+  }
+
+  onInputChange = event => {
+    this.setState({newTask: event.target.value})
+  }
+
+  //openModal = () => {
+  //  this.
+  //}
 
   componentDidMount() {
     this.getAndRenderTasks();
   }
 
-  //component List.js doesnt have access to getAndRenderTasks
-  //should there be no components?
+  componentWillUnmount() {
+    //trzeba usuwac usuwane z widoku elementy
+  }
+
   render() {
     return (    
       <main className="wrapper">
-        <Header />
+
+        <div id="addForm" className="header">
+          <h1>&lt; MY CHECKLIST &gt;</h1>
+          <form autoComplete="off">
+            <i className="fas fa-pen-alt"></i>
+            <input type="text"
+            //chcemy na każdym razem jak zmieniamy wartość inputa aktualizować nasz newTask w this.state
+            onChange={this.onInputChange}
+            />
+            <button className="addTodo"
+            onClick={() => this.postAndRenderTasks(this.state.newTask)}
+            >ADD</button>
+          </form>
+        </div>
+        
         <ul id="list"> 
         {
           this.state.todoList.map(task => {
             return (
             <li key={task.id}>
-              {task.title}
-              <button>delete</button>
+              
+              <span 
+              onClick={this.openModal}>
+                {task.title}
+                </span>
+
+              <button 
+              onClick={() => this.deleteAndRenderTasks(task.id)}>
+                delete
+                </button>
+
             </li>
-            );
+            )
           })
         }
         </ul>
-        <Modal />
+
+        <div className="modal">
+          <div className="modal-content">
+            <span id="closePopup" className="close">&times;</span>
+            <form id="popupForm" autoComplete="off">
+              <input type="text" id="popupInput" className="popupInput" placeholder="edit task"/>
+              <div className="popupBtns">
+                <button id="cancelTodo" className="addBtn">CANCEL</button>
+                <button id="acceptTodo" className="addBtn">OK</button>
+              </div>
+            </form>
+          </div>
+        </div>
+
       </main>
     );
   }
