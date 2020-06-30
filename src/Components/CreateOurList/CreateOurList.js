f5a054a185c053fed7a9cc6cebd2d2e814cf9339
zuzanna_brzozowska <zuzabrzozowska@gmail.com>
2020-06-30 15:56:19 +0200

    added clicked todo in Modal's input field and changed state of currentInputVal to edited value

--- src/Components/CreateOurList/CreateOurList.js
+++ src/Components/CreateOurList/CreateOurList.js
@@ -2,38 +2,45 @@ import React from 'react';
 import './CreateOurList.css';
 import Button from '../Button/Button.js';
 
-//FUNCTIONS REACT
-//1. props are in the arguments
-//2. if there is no state, no class needed (unless hooks)
-//3. only return, no render
-
 class CreateOurList extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            show: false
+            show: false,
+            currentInputVal: '',
+            span: ''
         }
+        
     }
     
-    showModal = () => {
+    showModal = event => {
         this.setState({ show: true });
+
+        const spanText = event.target.closest('li').firstElementChild.innerText;
+        this.setState({span: spanText})
     };
+
+    getEditedTask = event => {
+        const editedVal = event.target.value;
+        this.setState({currentInputVal: editedVal})
+        console.log(this.state.currentInputVal) //<-----rerender co kazda litere :(
+    }
     
     hideModal = () => {
         this.setState({ show: false });
     };
-    
+
     render() {
         return(
             <React.Fragment>
                 <ul id="list"> 
                     <h1>My todos:</h1>
                     {this.props.todoList.map(task => {
-                        return (
+                        return (  //jak podmienić {task.title} na {this.state.currentInputVal}
                             <li key={task.id}>
-                                
-                            <span className="todoTitle"> {task.title} </span>
+                        
+                            <span className="todoTitle"> {task.title} </span> 
                             { task.description && <span>description: {task.description}</span>}
                             { task.priority && <span>priority: {task.priority}</span>}
                             { task.url && <span><a target="_blank" rel="noopener noreferrer" href={task.url}>{task.url}</a></span>}
@@ -46,25 +53,26 @@ class CreateOurList extends React.Component {
                     })}
                 </ul>
 
-                <Modal show={this.state.show} handleClose={this.hideModal}/>
+                <Modal show={this.state.show} handleClose={this.hideModal} span={this.state.span} getEditedTask={this.getEditedTask}/>
                 
             </React.Fragment>
         );
     }
 }
 
-const Modal = ({ handleClose, show }) => {
+
+const Modal = ({ handleClose, show, span, getEditedTask }) => {
     const showHideClassName = show ? "modal display-block" : "modal";
-  
+
     return (
       <div className={showHideClassName}>
-        <div className="modal-content">
-                    <button onClick={handleClose} className="close">close</button>
-                    
-                    <form autoComplete="off">
-                        <input type="text" className="popupInput"/>
-                        <button className="addBtn">OK</button> 
-                    </form>
+        <div className="modal-content">  
+            <form autoComplete="off">
+                <input type="text" className="popupInput" defaultValue={span} onChange={getEditedTask}/>
+                <button className="addBtn">OK</button>
+            </form>
+            <button onClick={handleClose}>close</button>
+
         </div>
       </div>
     );
